569a2fbad52e7e28b3fc626053402ee6659aa5a5
Semih Orhan <sorhan3.14@gmail.com>
2023-12-22 21:37:31 +0300

    handle moderation

--- event-bus/index.js
+++ event-bus/index.js
@@ -19,6 +19,10 @@ app.post("/events", async (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
+
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 
