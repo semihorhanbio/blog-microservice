ac28f95cd8a9f36118fbd63e8e1051f337912e19
Semih Orhan <sorhan3.14@gmail.com>
2023-12-22 21:49:11 +0300

    update comment content

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,9 @@ app.post("/posts/:id/comments", async (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
+
   comments.push({ id: commentId, content, status: "pending" });
+
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
@@ -35,8 +37,31 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Received Event", req.body.type);
+app.post("/events", async (req, res) => {
+  console.log("Event Received:", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
   res.send({});
 });
 
