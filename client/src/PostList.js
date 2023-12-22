96d51cb6b89d7e0ceb2d01fbc74b71981a69bb72
Semih Orhan <sorhan3.14@gmail.com>
2023-12-22 19:58:54 +0300

    use query service

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -4,14 +4,17 @@ import CommentCreate from "./CommentCreate";
 import CommentList from "./CommentList";
 
 const PostList = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState({});
+
   const fetchPosts = async () => {
-    const response = await axios.get("http://localhost:4000/posts");
-    setPosts(response.data);
+    const res = await axios.get("http://localhost:4002/posts");
+
+    setPosts(res.data);
   };
+
   useEffect(() => {
     fetchPosts();
-  });
+  }, []);
 
   const renderedPosts = Object.values(posts).map((post) => {
     return (
@@ -22,7 +25,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
