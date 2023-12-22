2bf863a2b8f36474aecfffb4ec25b073e173ee65
Semih Orhan <sorhan3.14@gmail.com>
2023-12-22 22:05:29 +0300

    render comments by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,13 @@
 import React from "react";
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    comment.status === "approved"
+      ? (content = comment.content)
+      : comment.status === "rejected"
+      ? (content = "This comment has been rejected")
+      : (content = "This comment is awaiting moderation");
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
