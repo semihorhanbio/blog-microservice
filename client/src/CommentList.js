310690ab0ebcefb65e0a1e1a335e751803f597c7
Semih Orhan <sorhan3.14@gmail.com>
2024-03-21 14:32:30 +0300

    deploy react app

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,16 +1,24 @@
 import React from "react";
+
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     let content;
-    comment.status === "approved"
-      ? (content = comment.content)
-      : comment.status === "rejected"
-      ? (content = "This comment has been rejected")
-      : (content = "This comment is awaiting moderation");
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
     return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
 };
-
 export default CommentList;
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -3,9 +3,14 @@ import axios from "axios";
 
 const PostCreate = () => {
   const [title, setTitle] = useState("");
+
   const onSubmit = async (event) => {
     event.preventDefault();
-    await axios.post("http://localhost:4000/posts", { title });
+
+    await axios.post("http://posts.com/posts", {
+      title,
+    });
+
     setTitle("");
   };
 
